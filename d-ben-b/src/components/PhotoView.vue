a1e83d07b01e8e02db721861decc73000e30bf63
d-ben-b <155895903+d-ben-b@users.noreply.github.com>
2025-07-15 16:51:03 +0800

    Add PhotoView component and update Anniversary view with new photo data; remove old PhotoView file

---
+++ d-ben-b/src/components/PhotoView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="inset-0 z-10 items-center justify-center bg-black opacity-50 contaoner fix">
+    \
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  isVisible: Boolean,
+  project: Object,
+});
+
+const project = {
+
+}
+
+</script>
--- d-ben-b/src/views/Anniversary.vue
+++ d-ben-b/src/views/Anniversary.vue
@@ -3,20 +3,23 @@
     class="container relative flex w-full min-h-screen overflow-x-auto select-none active:cursor-grabbing"
     @mousedown="startDrag" @mousemove="onDrag" @mouseleave="stopDrag" @mouseup="stopDrag">
     <!-- Rope SVG（延伸進來用 dashed 線條） -->
+    <PhotoView />
     <svg class="absolute top-[15px] left-0 w-[120%] h-8 z-0" viewBox="0 0 600 50" preserveAspectRatio="none">
       <path d="M0,25 C100,5 200,45 300,25 C400,5 500,45 600,25" stroke="#8B5E3C" stroke-width="6" fill="none"
         stroke-linecap="round" stroke-dasharray="6,6" />
     </svg>
     <div class="flex gap-4 px-4 py-2 flex-nowrap top-[38px] absolute ">
       <PhotoCard class="card" v-for="(photo, index) in photos" :image="photo.image" :time="photo.date"
-        :title="photo.title" :key="index" />
+        :title="photo.title" :key="index" @click="openModal(index)" />
     </div>
+
   </div>
 </template>
 
 <script setup>
 import { ref, computed } from "vue";
 import PhotoCard from "@/components/PhotoCard.vue";
+import PhotoView from "@/components/PhotoView.vue"
 const scrollContainer = ref(null);
 let isDragging = false;
 let startX = 0;
@@ -25,46 +28,36 @@ let scrollLeft = 0;
 const image_files = [];
 const photos = [
   {
-    image: "https://picsum.photos/id/1011/600/600",
-    date: "2025-07-10",
-    title: "Ocean Breeze",
+    image: "anniversary/first/1-0.jpg",
+    date: "2022-08-30",
+    title: "Dating for the first time",
   },
   {
-    image: "https://picsum.photos/id/13/600/600",
-    date: "2025-07-11",
-    title: "Mountain Trail",
+    image: "anniversary/Anping/2-0.jpg",
+    date: "2022-09-11",
+    title: "Anping",
   },
   {
-    image: "https://picsum.photos/id/5/600/600",
-    date: "2025-07-12",
-    title: "City Lights",
+    image: "anniversary/Helloween/3-0.jpg",
+    date: "2022-10-28",
+    title: "Helloween",
   },
   {
-    image: "https://picsum.photos/id/51/600/600",
-    date: "2025-07-12",
-    title: "City Lights",
+    image: "anniversary/Chrismas/4-0.jpg",
+    date: "2022-12-25",
+    title: "Chrismas",
   },
   {
     image: "https://picsum.photos/id/115/600/600",
     date: "2025-07-12",
     title: "City Lights",
   },
-  {
-    image: "https://picsum.photos/id/255/600/600",
-    date: "2025-07-12",
-    title: "City Lights",
-  },
+
 ];
 
-const image = computed(() => {
-  return (index) => {
-    if (index < 0 || index >= image_files.length) {
-      console.error(`Invalid index: ${index}`);
-      return null;
-    }
-    return new URL(`../assets/images/${image_files[index]}`, import.meta.url);
-  };
-});
+const openModal = (index) => {
+
+}
 
 const startDrag = (e) => {
   isDragging = true;
